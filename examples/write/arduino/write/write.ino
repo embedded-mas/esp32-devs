cdccbdaeda2e4dadc7d8dfb172555d048c9c285f
joao puel <you@example.com>
2022-09-09 16:11:14 -0300

    Update

---
+++ examples/write/arduino/write/write.ino
@@ -0,0 +1,22 @@
+/* sudo chmod a+rw /dev/ttyUSB0 */
+
+const int LIGHT_A = 9;
+const int LIGHT_B = 10;
+void setup() 
+{
+  
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  Serial.println("teste");
+  while (Serial.available() > 0 ) {
+
+     String str = Serial.readString();
+
+     delay(10000);
+     Serial.println(str);
+  }
+  delay(1000);
+}
